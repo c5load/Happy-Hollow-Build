e8efb4b1306c88d281c21b2dba7f726a3c937e2c
Anthony Puah <Anthony@Anthony-PC.(none)>
2012-02-25 23:10:10 -0800

    2/25 23:10-Schedule js's

---
+++ Resources/Schedule/events.js
@@ -0,0 +1,108 @@
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformHeight;
+
+var win = Ti.UI.currentWindow;
+
+var TitleBar=Titanium.UI.createImageView({
+	image:'schedulebackground.png',
+    width: pWidth,
+    left: '0dp',
+    top: '0dp',
+    height: pHeight*.1
+});
+
+var lblTitle=Titanium.UI.createLabel({
+	text:"Schedule",
+	textAlign:'center',
+	color:'white',
+	font:{
+		fontSize:'30dp',
+		fontWeight:'bold',
+	},
+	width: pWidth,
+	left:'0dp', 
+    top: '0dp',
+    height: pHeight*.1
+});
+
+var buttonHome = Titanium.UI.createButton({
+	color:'#fff',
+	backgroundImage:'homeresting.png',
+	backgroundSelectedImage:'homeselected.png',
+	top: pHeight*.02, 
+	left:pWidth*.04,
+	width:pWidth*.19,
+	height:pHeight*.07,});
+buttonHome.addEventListener('click', function()
+{var winHomeScreen=Titanium.UI.createWindow({
+    title:'Happy Hollow Park and Zoo',
+    backgroundColor:'#FFFFFF',
+    url: '..app.js',
+    navBarHidden:true,
+    fullscreen : true,  
+    navBarHidden: true
+});
+	winHomeScreen.open();});
+
+
+
+var winEventsLabel = Ti.UI.createLabel({
+	title:'Events',
+	backgroundImage: 'schedulebackground.png',
+	textWeight:'strong',
+	textAlign: pWidth*.1,
+	color: '#fff',
+    font: {
+        fontSize: '25dp',
+        fontWeight: 'normal'
+    },
+    width: pWidth,
+    textAlign: 'left',
+    left: '0dp',
+    top: pHeight*.55,
+    height: pHeight*.15
+})
+
+
+if (win.pictureURL ==='None')
+	//don't display a picture
+	{}
+	//otherwise create and display an imageView
+	else{ var image = Titanium.UI.createImageView({
+	url:win.pictureURL,
+	width:pWidth,
+	height:pHeight*.5,
+	top:pHeight*.1,
+	left:'0dp'});
+	
+	win.add(image);
+	};
+	
+	var scrollView = Titanium.UI.createScrollView({ 
+		contentWidth:'auto', 
+		contentHeight:'auto', 
+		top:pHeight*.7, 
+		showVerticalScrollIndicator:true, 
+		showHorizontalScrollIndicator:true }); 
+		
+	var winDescription = Ti.UI.createLabel({
+	text: 'We have several events going on at Happy Hollow Park & Zoo!',
+	textAlign: 'center',
+	color: 'black',
+    font: {
+        fontSize: '18dp',
+        fontWeight: 'normal'
+    },
+    width: pWidth,
+    left: '0dp',
+    top: '0dp',
+    height: 'auto'
+})
+		scrollView.add(winDescription);
+		Titanium.UI.currentWindow.add(scrollView);
+		
+
+win.add(TitleBar);
+win.add(lblTitle);
+win.add(buttonHome);
+win.add(winEventsLabel);
