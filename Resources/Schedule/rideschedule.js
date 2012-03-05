3df8e1c110f020140ed5fb54f3c815b6c54bbf89
Mark Myers <c5load@gmail.com>
2012-03-05 15:10:30 -0800

    Updated schedules

--- Resources/Schedule/rideschedule.js
+++ Resources/Schedule/rideschedule.js
@@ -26,43 +26,62 @@ var lblTitle=Titanium.UI.createLabel({
     height: '50dp'
 });
 
-var xhr = Titanium.Network.createHTTPClient();
 
-xhr.onload = function()
-{
-    var data = [];
-    var doc = this.responseXML.documentElement;
-    var elements = doc.getElementsByTagName("times");
-    for (var i=0;i<elements.length;i++) {
-        var row = Ti.UI.createTableViewRow({
-        	height:'80dp'
-        });
-        row.title = elements.item(i).getAttribute("Times");
-        hour = doc.getElementsByTagName("time").item(i).text;
 
-        var hoursLabel = Ti.UI.createLabel({
-        	text: hour,
-        	color:'#000000',
-        	font:{fontSize:'20dp'},
-        	textAlign:'left',
-        	left:'100dp'
+
+var data = [];
+
+var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,'rideinfo.xml');
+var xmltext = file.read().text;
+var doc = Ti.XML.parseString(xmltext);
+var elements = doc.getElementsByTagName("ride");
+
+for (var i=0;i<elements.length;i++) {
+    var row = Ti.UI.createTableViewRow({
+    	height:'80dp'
+    });
+    row.title = elements.item(i).getAttribute("Title");
+    title = doc.getElementsByTagName("title").item(i).text;
+    var hours = [];   
+    var titleLabel = Ti.UI.createLabel({
+    	text: title,
+    	color:'#000000',
+    	font:{fontSize:'20dp'},
+    	textAlign:'left',
+    	left:'0dp'
         });
-        
-        row.add(hoursLabel);
+
+        row.add(titleLabel);
         data.push(row);}
    
     var tableview = Titanium.UI.createTableView({
        top:'50dp',
-       data:data,
-       height:'auto',
-    });
-    tableview.setData(data);
-    Titanium.UI.currentWindow.add(tableview); 
- 
+   data:data,
+   height:'auto',
+});
+tableview.setData(data);
+Titanium.UI.currentWindow.add(tableview); 
 
-};
+tableview.addEventListener('click',function(e)
+		{
+			var w = Ti.UI.createWindow({
+			url:'specificride.js', 
+			navBarHidden:true, 
+   			title:'',
+   			backgroundcolor:'black',
+   			fullscreen:true });
+			var b = Titanium.UI.createButton({
+				title:'Close',
+				style:Titanium.UI.iPhone.SystemButtonStyle.PLAIN
+			});
+			w.setLeftNavButton(b);
+			b.addEventListener('click',function()
+			{
+				w.close();
+			});
+			w.open({fullscreen:true});
+		});
+
 
 win.add(TitleBar);
 win.add(lblTitle);
-xhr.open('GET','http://hhpz.org/mobile/cf/rideinfo.cfm');
-xhr.send();//declare the http client object
